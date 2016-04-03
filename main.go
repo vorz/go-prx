4d80c2f96cb50ab668916d0803336f36a9353f60
Kilgore Trout <trout85@yandex.ru>
2016-04-03 22:54:28 +0300

    Start

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+	"os"
+	"runtime"
+	"time"
+
+	"Proxy/prx"
+)
+
+var (
+	port = flag.String("port", "8080", "Port for proxy")
+	dbg  = flag.Bool("d", false, "Debug information")
+)
+
+func main() {
+	// Просим Go использовать все имеющиеся в системе процессоры.
+	runtime.GOMAXPROCS(runtime.NumCPU())
+
+	//logFile, _ := os.Create("logs.txt")
+
+	//serv := http.Server{}
+
+	logger := log.New(os.Stdout, "log: ", log.Ldate|log.Ltime)
+	logger.Printf("Сервер запущен: %v", time.Now())
+
+	pServ := &prx.ProxyServ{Log: logger}
+
+	logger.Fatal(http.ListenAndServe(":"+*port, pServ))
+}
---
+++ prx/proxy.go
@@ -0,0 +1,172 @@
+package prx
+
+import (
+	"io"
+	"io/ioutil"
+	"log"
+	"net"
+	"net/http"
+	"time"
+)
+
+var tr = &http.Transport{
+	DisableCompression: true,
+	DisableKeepAlives:  true,
+	//TLSClientConfig:    tlsClientSkipVerify,
+}
+
+//ProxyServ is for FUCKSAKE
+type ProxyServ struct {
+	Log   *log.Logger
+	Debug bool
+}
+
+//Стандартная функция ServeHTTP интерфейса Handler
+func (p *ProxyServ) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	p.Debugf("-------Получен запрос %v %v %v %v", r.URL.Path, r.Host, r.Method, r.URL.String())
+	if r.Method == "CONNECT" {
+		p.handleConnect(w, r)
+	} else {
+		p.handleHTTP(w, r)
+	}
+
+}
+
+//Функция обработки обычных http запросов
+func (p *ProxyServ) handleHTTP(w http.ResponseWriter, r *http.Request) {
+	p.Debugf("handleHttp. Запущена обработка http-соединения с методом %v", r.Method)
+
+	start := time.Now()
+
+	/*
+		for key, val := range r.Header {
+			p.Log.Print(key, " : ", val)
+		}
+		p.Log.Print(r.Body)
+
+		p.Log.Print("Host: ", r.Host, " ; ", " Is ABS: ", r.URL.IsAbs())
+		p.Log.Print("Remote: ", r.RemoteAddr, " ; ", " URL RequestURI: ", r.URL.RequestURI())
+		p.Log.Print("Content Length: ", r.ContentLength)
+	*/
+	r.RequestURI = ""
+	r.Header.Del("Accept-Encoding")
+	if _, ok := r.Header["Proxy-Connection"]; ok {
+		r.Header.Set("Connection", r.Header["Proxy-Connection"][0])
+	}
+	r.Header.Del("Proxy-Authenticate")
+	r.Header.Del("Proxy-Authorization")
+	r.Header.Del("Proxy-Connection")
+
+	resp, err := tr.RoundTrip(r)
+	if err != nil {
+		p.Warnf("ОШИБКА при пересылке: %s", err.Error())
+		http.Error(w, "Ошибка доступа к удаленному сайту: "+err.Error(), 404)
+		return
+	}
+	if resp == nil {
+		p.Warnf("ОШИБКА получения ответа от сервера %v %v:", r.URL.Host, err.Error())
+		http.Error(w, "ОШИБКА получения ответа от сервера: "+err.Error(), 500)
+		return
+	}
+	p.Debugf("Получен ответ от %v: %v", r.URL.Host, resp.Status)
+
+	for k := range w.Header() {
+		w.Header().Del(k)
+	}
+
+	for k, v := range resp.Header {
+		for _, vv := range v {
+			w.Header().Add(k, vv)
+		}
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		p.Warnf("ОШИБКА чтения тела ответа %s", err.Error())
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	if err := resp.Body.Close(); err != nil {
+		p.Warnf("ОШИБКА: невозможно закрыть http.Body. %v", err)
+	}
+
+	w.WriteHeader(resp.StatusCode)
+	w.Write(body)
+
+	finish := time.Now()
+	duration := finish.Sub(start)
+
+	p.Warnf("%v:  %v. %d байт за %v", r.Method, r.URL.Host, len(body), duration)
+
+}
+
+//Функция туннелирования CONNECT-запросов (https)
+func (p *ProxyServ) handleConnect(w http.ResponseWriter, r *http.Request) {
+	p.Debugf("=====handleConnect. Запущена обработка CONNECT")
+
+	start := time.Now()
+
+	conn, _, err := w.(http.Hijacker).Hijack()
+	if err != nil {
+		p.Panicf("Proxy hijacking not supporting %s: %s", r.RemoteAddr, err.Error())
+		return
+	}
+
+	_, err = io.WriteString(conn, "HTTP/1.0 200 Connection established\r\n\r\n")
+	if err != nil {
+		p.Warnf("ОШИБКА: Невозможно отправить ответ %s: %s", r.RemoteAddr, err.Error())
+		return
+	}
+
+	dstConn, err := net.Dial("tcp", r.URL.Host)
+	if err != nil {
+		p.Warnf("ОШИБКА: Невозможно соединиться с %s: %s", r.RequestURI, err.Error())
+		return
+	}
+
+	var done = make(chan int64)
+
+	//Функция копирования и закрытия соединения
+	fCopy := func(from, to net.Conn) {
+		n, err := io.Copy(to, from)
+		if err != nil {
+			p.Warnf("ОШИБКА копирования %s", err.Error())
+		}
+		done <- n
+	}
+
+	go fCopy(dstConn, conn)
+	go fCopy(conn, dstConn)
+
+	num := <-done
+	num += <-done
+
+	if err := conn.Close(); err != nil {
+		p.Warnf("ОШИБКА закрытия оригинального соединения %s", err.Error())
+	}
+	if err := dstConn.Close(); err != nil {
+		p.Warnf("ОШИБКА закрытия соединения с удаленным узлом %s", err.Error())
+	}
+
+	finish := time.Now()
+	duration := finish.Sub(start)
+
+	p.Warnf("%v:  %v. %d байт за %v", r.Method, r.URL.Host, num, duration)
+}
+
+//Debugf вывод для отладки если задан параметр Debug
+func (p *ProxyServ) Debugf(msg string, argv ...interface{}) {
+	if p.Debug {
+		p.Log.Printf(msg, argv...)
+	}
+}
+
+//Warnf вывод при ошибках
+func (p *ProxyServ) Warnf(msg string, argv ...interface{}) {
+	p.Log.Printf(msg, argv...)
+}
+
+//Panicf вывод при критических ошибках (завершение программы)
+func (p *ProxyServ) Panicf(msg string, argv ...interface{}) {
+	p.Log.Fatalf("КРИТИЧЕСКАЯ ОШИБКА:"+msg, argv...)
+}
